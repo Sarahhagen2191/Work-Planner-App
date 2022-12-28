0e049339400e2935e078e89536461f901bf2890f
Sarah hagen <sarahhagen@sarahs-mbp.home>
2022-12-28 14:17:03 +0000

    updade blocks/js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,32 @@
 //Display the current date at the top of the application 
+var $currentDay = $("#currentDay");
+var $timeBlocks = $(".time-block");
 
+var currentDate = moment().format("dddd, MMMM Do");
+var currentHour = moment().format("H");
+console.log(currentHour);
+currentHour = 13;
 
 // Create the time blocks for each hour of the working day 9 - 5
+$currentDay.text(currentDate); 
+
+$timeBlocks.each(function(){
+  var $thisBlock = $(this);
+  console.log($thisBlock.attr("data-hour"));
+  console.log(currentHour);
+
+  if( $thisBlock.attr("data-hour") == currentHour){
+    $thisBlock.addClass("present").removeClass("past future");
+  }
+  if ($thisBlock.attr("data-hour") < currentHour){
+    $thisBlock.addClass("past").removeClass("present future");
+  }
+  if ($thisBlock.attr("data-hour") > currentHour){
+
+    $thisBlock.addClass("future").removeClass("past present");
+  }
+});
+
 
 
 // Colour code the time block based on the curent time - using css classes? 
@@ -14,3 +39,10 @@
 //Bring any data from localStorage into the timeblocks when the page loads
 
 
+
+
+
+
+// var thisMoment = moment().format();
+// console.log(moment());
+// console.log(thisMoment);
