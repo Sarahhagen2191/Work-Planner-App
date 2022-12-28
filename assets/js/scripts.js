fe1af7b0ecaf57576307bc3d3a0115b9d60b784a
Sarah hagen <sarahhagen@sarahs-mbp.home>
2022-12-28 13:10:30 +0000

    Pseudo-Coding

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+//Display the current date at the top of the application 
+
+
+// Create the time blocks for each hour of the working day 9 - 5
+
+
+// Colour code the time block based on the curent time - using css classes? 
+
+// allow the user to add text to the textarea 
+
+//save the event via local storage
+
+
+//Bring any data from localStorage into the timeblocks when the page loads
+
+
