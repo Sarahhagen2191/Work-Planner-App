00b4ac78d371ad1bdad642baea94078cb32163c4
Sarah hagen <sarahhagen@sarahs-mbp.home>
2022-12-28 15:51:17 +0000

    update the JS logic as localstorage wasn't working

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Work-Planner-App
+
+# Description
+This is a calendar application that allows a user to save events for each hour of the day, crafted with JavaScript, jQuery, MomentJS, HTML and CSS, and it's using localStorage to save the data logged by the user.
+
+Link to the live version - 
+
+# Table of Contents: 
+- Screenshoots 
+-Installation 
+- Usuage 
+- Credit 
+- License 
+
+# Screenshots
+
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,98 +4,93 @@ var $scheduleArea = $(".schedule");
 
 var toDoItems = [];
 
-//each object has a hour property and a text property
 
+//Display the current date at the top of the application 
+ 
 var currentDate = moment().format("dddd, MMMM Do");
 var currentHour = moment().format("H");
 
+//if we don't have any todos set up, let's set up the array of objects
+function initializeSchedule(){
 
-//Display the current date at the top of the application 
-$currentDay.text(currentDate); 
+//for each time block
+  $timeBlocks.each(function(){
+    var $thisBlock = $(this);
+    var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
 
-// Colour code the time block based on the curent time - using css classes? 
-$timeBlocks.each(function(){
-  var $thisBlock = $(this);
-  var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+    var todoObj = {
+      //set related todo hour to same as data-hour
+      hour: thisBlockHr,
+      //get text ready to accept string input
+      text: "",
+    }
+    //add this todo object to todoitems array
+    toDoItems.push(todoObj);
+  });
 
-  if( thisBlockHr == currentHour){
-    $thisBlock.addClass("present").removeClass("past future");
-  }
-
-  if (thisBlockHr < currentHour){
-    $thisBlock.addClass("past").removeClass("present future");
-  }
-
-  if (thisBlockHr > currentHour){
-    $thisBlock.addClass("future").removeClass("past present");
-  }
-});
+  localStorage.setItem("todos", JSON.stringify(toDoItems));
+}
 
 
-/*var todos = localStorage.getItem("todos");
-var parsedTodos = JSON.parse(todos);
-console.log(parsedTodos);*/
-function initializeTodos(){
-    //just in case we want to update with more timeblock, i will not hardcode timeblocks num here
+function setUpTimeBlocks(){
     $timeBlocks.each(function(){
       var $thisBlock = $(this);
-      var toDoObject = {};
-      toDoObject.hour = parseInt($thisBlock.attr("data-hour"));
-      toDoItems.push(toDoObject);
-    }); //this might not be the best bc this is will be set if theres nothing in local storage... but if there IS something in local storage.
-  
-    localStorage.setItem("todos", JSON.stringify(toDoItems));
-    //console.log(toDoItems);
-  }
+      var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
 
-  function renderSchedule(){toDoItems = localStorage.getItem("todos");
+      // Colour code the time block based on the curent time 
+      if (thisBlockHr == currentHour) {
+        $thisBlock.addClass("present").removeClass("past future");
+      }
+      if (thisBlockHr < currentHour) {
+        $thisBlock.addClass("past").removeClass("present future");
+      }
+      if (thisBlockHr > currentHour) {
+        $thisBlock.addClass("future").removeClass("past present");
+      }
+    });
+}
+
+function renderSchedule(){
+  
+  toDoItems = localStorage.getItem("todos");
   toDoItems = JSON.parse(toDoItems);
 
-  //loop thru array then assign the text to the timeBlock with data-hour equal to hour. 
-  //make a variable where [data-hour={hour}] then plug it in to the selector $('[data-hour={hour}')
   for (var i = 0; i < toDoItems.length; i++){
     var itemHour = toDoItems[i].hour;
     var itemText = toDoItems[i].text; 
-    console.log(itemHour + "|" + itemText);
+   
     $("[data-hour=" + itemHour + "]").children("textarea").val(itemText);
   }
 
+  console.log(toDoItems);
 }
+
 
 function saveHandler(){
-    //event.preventDefault();
-    if (event.target.matches("button")){
-      var hourToUpdate = event.target.parentElement.getAttribute("data-hour");
-      var itemToAdd = (($(event.target).parent()).children("textarea")).val();
-      //i was having issues bc i was mixing javascript selectors with jquery functions... lets fix his
-      //see which item we need to update based on the hour of the button clicked matching
-      for (var j = 0; j < toDoItems.length; j++){
-        if (toDoItems[j].hour == hourToUpdate){
-          //set its text to what was added to textarea
-          toDoItems[j].text = itemToAdd;
-        }
-      }
-      localStorage.setItem("todos", JSON.stringify(toDoItems));
-      renderSchedule();
-    }
+  var $thisBlock = $(this).parent();
 
+  var hourToUpdate = $(this).parent().attr("data-hour");
+  var itemToAdd = (($(this).parent()).children("textarea")).val();
+
+  //see which item we need to update based on the hour of the button clicked matching
+  for (var j = 0; j < toDoItems.length; j++){
+    if (toDoItems[j].hour == hourToUpdate){
+      //set its text to what was added to textarea
+      toDoItems[j].text = itemToAdd;
+    }
+  }
+  localStorage.setItem("todos", JSON.stringify(toDoItems));
+  renderSchedule();
 }
 
-//add event listener to buttons
-$().ready(function(){
-  //initializeTodos();
+// when the document loads
+$(document).ready(function(){
+  setUpTimeBlocks();
+  if(!localStorage.getItem("todos")){
+    initializeSchedule();
+  } 
+  $currentDay.text(currentDate);
   renderSchedule();
-  $scheduleArea.click(saveHandler);
-
+  $scheduleArea.on("click", "button", saveHandler);
+  
 });
-
-// Colour code the time block based on the curent time - using css classes? 
-
-// allow the user to add text to the textarea 
-
-//save the event via local storage
-
-
-//Bring any data from localStorage into the timeblocks when the page loads
-
-
